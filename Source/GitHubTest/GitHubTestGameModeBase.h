2513f156f17b0f6726b8ed793b847edb29da7a63
XionsFlame <69725309+XionsFlame@users.noreply.github.com>
2020-08-15 10:45:12 -0700

    Initial commit

---
+++ Source/GitHubTest/GitHubTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GitHubTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITHUBTEST_API AGitHubTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
